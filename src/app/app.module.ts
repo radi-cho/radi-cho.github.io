f48df7be272a2f11de37eae14753e32fafc528b7
Radi Cho <radicho123@gmail.com>
2018-08-08 20:41:56 +0300

    Add projects component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { GreetingComponent } from './components/greeting/greeting.component';
 import { BasicInfoComponent } from './components/basic-info/basic-info.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 @NgModule({
-  declarations: [AppComponent, GreetingComponent, BasicInfoComponent],
+  declarations: [AppComponent, GreetingComponent, BasicInfoComponent, ProjectsComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
